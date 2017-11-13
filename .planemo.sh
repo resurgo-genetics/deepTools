e15d16dc61075567e7cad1923776b01c74e1648b
dpryan79 <dpryan79@gmail.com>
2017-11-13 16:46:24 +0100

    We shouldn't now need to modify Galaxy, the deepTools datatypes are built into 17.09

--- .planemo.sh
+++ .planemo.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-blah=`mktemp -d`
-conda config --add channels conda-forge
-conda config --add channels bioconda
-/home/travis/build/deeptools/deepTools/foo/bin/planemo database_create galaxy
+#blah=`mktemp -d`
+#conda config --add channels conda-forge
+#conda config --add channels bioconda
+#/home/travis/build/deeptools/deepTools/foo/bin/planemo database_create galaxy
 
-git clone --depth 1 --single-branch --branch release_16.10 https://github.com/galaxyproject/galaxy.git clone
-cd clone
-touch tool-data/twobit.loc
-#Add the custom data types
-sed -i '4i\    <datatype extension="deeptools_compute_matrix_archive" type="galaxy.datatypes.binary:CompressedArchive" subclass="True" display_in_upload="True"/>' config/datatypes_conf.xml.sample
-sed -i '5i\    <datatype extension="deeptools_coverage_matrix" type="galaxy.datatypes.binary:CompressedArchive" subclass="True" display_in_upload="True"/>' config/datatypes_conf.xml.sample
-#Remove numpy and decorator (conflicts with deepTools/plotly)
-sed -i '7d;55d' lib/galaxy/dependencies/pinned-requirements.txt
-./scripts/common_startup.sh --skip-venv --dev-wheels
-cd ..
-pip install .
+#git clone --depth 1 --single-branch --branch release_16.10 https://github.com/galaxyproject/galaxy.git clone
+#cd clone
+#touch tool-data/twobit.loc
+##Add the custom data types
+#sed -i '4i\    <datatype extension="deeptools_compute_matrix_archive" type="galaxy.datatypes.binary:CompressedArchive" subclass="True" display_in_upload="True"/>' config/datatypes_conf.xml.sample
+#sed -i '5i\    <datatype extension="deeptools_coverage_matrix" type="galaxy.datatypes.binary:CompressedArchive" subclass="True" display_in_upload="True"/>' config/datatypes_conf.xml.sample
+##Remove numpy and decorator (conflicts with deepTools/plotly)
+#sed -i '7d;55d' lib/galaxy/dependencies/pinned-requirements.txt
+#./scripts/common_startup.sh --skip-venv --dev-wheels
+#cd ..
+#pip install .
 #galaxy/wrapper/correctGCBias.xml \
-/home/travis/build/deeptools/deepTools/foo/bin/planemo test --galaxy_root clone --test_data galaxy/wrapper/test-data/ --skip_venv --postgres --no_conda_auto_install --no_conda_auto_init --no_dependency_resolution \
+/home/travis/build/deeptools/deepTools/foo/bin/planemo test --test_data galaxy/wrapper/test-data/ --postgres --no_conda_auto_install --no_conda_auto_init --no_dependency_resolution \
 galaxy/wrapper/bamCompare.xml \
 galaxy/wrapper/bamCoverage.xml \
 galaxy/wrapper/bamPEFragmentSize.xml \
